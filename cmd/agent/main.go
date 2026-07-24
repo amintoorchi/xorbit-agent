8c73e9fc585ee4bc3293b492c2986a93e1976151
Amin Toorchi <amin.tourchie@gmail.com>
2026-07-24 13:59:08 +0330

    attach auth middlware

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,16 +4,24 @@ import (
 	"log"
 
 	"go-agent/internal/api"
+	"go-agent/internal/config"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := gin.Default()
 
 	if err := router.SetTrustedProxies(nil); err != nil {
 		log.Fatal(err)
 	}
+
+	router.Use(api.AuthMiddleware(cfg.Token))
 
 	api.RegisterRoutes(router)
 
